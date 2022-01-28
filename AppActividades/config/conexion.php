88b8e0cbfc63b70db07f2f306309de27b4cb69f7
ACG TICS 03 <98350329+acgtics03@users.noreply.github.com>
2022-01-28 11:14:02 -0500

    Update conexion.php

--- AppActividades/config/conexion.php
+++ AppActividades/config/conexion.php
@@ -1,26 +1,26 @@
-<?php 
-     
+<?php
+
 	/**
-	 * 
+	 *
 	 */
-	class Conexion 
+	class Conexion
 	{
-		
+
 		public static function Conectar()
-		{	
+		{
 			/*
 		    $PDO = new PDO("mysql:host=localhost;dbname=acgsoft_Nominas;charset=utf8","acgsoft_appVisitas","adm2019acg");
 			$PDO->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 			return $PDO;
-		*/	
-		   	$PDO = new PDO("mysql:host=localhost;dbname=acgsoft_nominas;charset=utf8","root","");
+		*/
+		   	$PDO = new PDO("mysql:host=localhost;dbname=acgsoft_appvisitas;charset=utf8","root","");
 			$PDO->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 			return $PDO;
-		
+
 		/* $PDO = new PDO("mysql:host=localhost;dbname=acgsoft_nominas;charset=utf8","root","1234");
 			$PDO->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 			return $PDO;*/
-			
+
 		}
 	}
 
